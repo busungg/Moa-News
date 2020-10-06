765c0e01f4b697875b6d1e4772ab104f68ccf83d
busungg <busungg@naver.com>
2020-10-06 22:49:51 +0900

    #27 CORS 문제 해결 1. 426 Error 확인 및 코드 원복

--- src/utils/http/index.js
+++ src/utils/http/index.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
-//const axios = require('axios').default;
-
 export const httpGet = async (url, params, headers) => {
   const response = await axios.get(url, {
     params: {
       ...params,
     },
-    //responseType: 'json',
+    responseType: 'json',
     headers: {
       ...headers,
     },
